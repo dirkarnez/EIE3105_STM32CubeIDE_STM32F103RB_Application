7fb869418a1769d5c4791126c508596b8424a69d
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-19 18:48:41 +0800

    - update local-burn.cmd

--- EIE3105_STM32CubeIDE_STM32F103RB_Application/local-burn.cmd
+++ EIE3105_STM32CubeIDE_STM32F103RB_Application/local-burn.cmd
@@ -3,11 +3,15 @@ REM run as Administrator
 
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
-set Software_DIR=D:\Softwares
+
+set SOFTWARES_DIR=D:\Softwares
+set PROGRAM_FILES=C:\"Program Files"
 
 SET PATH=^
 %DOWNLOADS_DIR%\STM32CubeIDE-portable-v1.6.1\plugins\com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.win32_2.1.400.202404281720\tools\bin;^
-%Software_DIR%\STM32CubeIDE-portable-v1.6.1\plugins\com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.win32_2.1.400.202404281720\tools\bin;
+%SOFTWARES_DIR%\STM32CubeIDE-portable-v1.6.1\plugins\com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.win32_2.1.400.202404281720\tools\bin;^
+%PROGRAM_FILES%\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin;
 
 cd .ci && ( (  STM32_Programmer_CLI.exe -c port=SWD freq=4000 -w EIE3105_STM32CubeIDE_STM32F103RB_Application.elf -s && echo ok ) || echo failed ) && pause
+
 
